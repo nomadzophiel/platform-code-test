16f5a787b9c3d70c7121b9fa54ce20b84d16f496
Matt <asullins@bartech.com>
2021-12-01 14:50:45 -0800

    initial commit

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,63 @@
 require 'award'
 
+#Completed project for https://github.com/mdx-dev/platform-code-test
+
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
+    non_degrading_award = false
+    increasing_award = false
+    if award.name == 'Blue First' || award.name == 'Blue Compare' || award.name == 'Blue Distinction Plus'
+      non_degrading_award = true
+    end
+    if award.name == 'Blue First' || award.name == 'Blue Compare'
+      increasing_award = true
+    end
+
+    if award.quality < 50 && increasing_award == true
+      if award.name == 'Blue First'
+        process_blue_first(award)
+      elsif award.name == 'Blue Compare'
+        process_blue_compare(award)
       end
     end
+
+    if non_degrading_award != true && award.quality > 0
+      degrade(award)
+    end
+    if award.name == 'Blue Star' && award.quality > 0
+      degrade(award)
+    end
+
     if award.name != 'Blue Distinction Plus'
       award.expires_in -= 1
     end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+  end
+end
+
+def process_blue_first(award)
+  award.quality += 1
+  if award.expires_in <= 0 && award.quality < 50
+    award.quality += 1
+  end
+end
+
+def process_blue_compare(award)
+  if award.expires_in > 0 && award.quality < 50
+    award.quality += 1
+    if award.expires_in < 11 && award.quality < 50
+      award.quality += 1
     end
+    if award.expires_in < 6 && award.quality < 50
+      award.quality += 1
+    end
+  else
+    award.quality = 0
+  end
+end
+
+def degrade(award)
+  award.quality -= 1
+  if award.expires_in <1 && award.quality > 0
+    award.quality -= 1
   end
 end
